6bab68b49c938532c632d0c41119aa9e0f3fd62f
work-with-ekongg <officially.ekongg@gmail.com>
2026-02-12 21:20:53 +0100

    Add files via upload

---
+++ data/projects.py
@@ -0,0 +1,18 @@
+projects = [
+    {
+        "title": "School Website",
+        "description": "A modern school website with admissions and responsive design.",
+        "tech": ["Python"],
+        "image": "/images/school.jpg",
+        "github": "https://github.com/work-with-ekongg/TEEVES-SCHOOL",
+        "live": "https://teeves-school.onrender.com"
+    },
+    {
+        "title": "Restaurant Project",
+        "description": "A Modern, responsive restaurant website",
+        "tech": ["Python"],
+        "image": "/images/burritos.jpg",
+        "github": "https://github.com/work-with-ekongg/",
+        "live": "https://restaurant-by-britishh.onrender.com"
+    }
+]
